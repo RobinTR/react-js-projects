80f4c4716983991301fd5ab16d38542351bcd405
Mem Robin Çen <memrobincen@gmail.com>
2025-07-21 19:16:03 +0300

    feat: refactor routes to show example of use prefix for custom routes

--- react/react-router-v7-examples/app/routes.ts
+++ react/react-router-v7-examples/app/routes.ts
@@ -1,9 +1,10 @@
-import { type RouteConfig, index, layout, route } from "@react-router/dev/routes";
+import { type RouteConfig, index, layout, prefix, route } from "@react-router/dev/routes";
 
 export default [
     index("routes/home.tsx"),
-    route("about", "routes/about.tsx"),
-    route("post/:postId", "routes/post.tsx"),
+    ...prefix("custom", [
+        route("about", "routes/about.tsx"),
+        route("post/:postId", "routes/post.tsx")]),
 
     //Nested Routes
     route("dashboard", "routes/dashboard.tsx", [
@@ -16,5 +17,5 @@ export default [
         route("finances", "routes/finances.tsx"),
         route("personal-info", "routes/personal-info.tsx")
     ])
-        */
+    */
 ] satisfies RouteConfig;
