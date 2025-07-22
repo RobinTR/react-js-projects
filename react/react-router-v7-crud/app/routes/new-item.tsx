cdba61912e5ca58a0be75f5fdad6f194233372e2
Mem Robin Çen <memrobincen@gmail.com>
2025-07-22 18:46:14 +0300

    feat: implement new item creation form

--- react/react-router-v7-crud/app/routes/new-item.tsx
+++ react/react-router-v7-crud/app/routes/new-item.tsx
@@ -1,3 +1,42 @@
+import { Form, redirect } from "react-router";
+import type { Route } from "./+types/new-item";
+import { supabase } from "~/supabase-client";
+
+export async function action({ request }: Route.ActionArgs) {
+  const formData = await request.formData();
+  const title = formData.get("title") as string;
+  const description = formData.get("description") as string;
+
+  if (!title || !description) {
+    return { error: "Title and content are required" };
+  }
+
+  const { error } = await supabase.from("items").insert({ title, description });
+
+  if (error) {
+    return { error: error.message };
+  }
+
+  return redirect("/");
+}
+
 export default function NewItem() {
-    return <div>Items Page</div>
+  return (
+    <div>
+      <h2>Create New Item</h2>
+      <Form method="post">
+        <div>
+          <label>Title</label>
+          <input type="text" name="title" required />
+        </div>
+
+        <div>
+          <label>Description</label>
+          <textarea name="description" required />
+        </div>
+
+        <button type="submit">Create Item</button>
+      </Form>
+    </div>
+  );
 }
