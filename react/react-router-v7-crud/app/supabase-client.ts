2a8fda9daba5b35c5a4ab5043e3674586556e2f4
Mem Robin Çen <memrobincen@gmail.com>
2025-07-22 20:35:01 +0300

    feat: update Supabase client to use environment variables for configuration. (Keys reseted)

--- react/react-router-v7-crud/app/supabase-client.ts
+++ react/react-router-v7-crud/app/supabase-client.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const SUPABASE_URL = "https://skururrmwlzhhjwnmmwl.supabase.co";
-const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNrdXJ1cnJtd2x6aGhqd25tbXdsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTMxOTQxNjYsImV4cCI6MjA2ODc3MDE2Nn0.xzqNVcL2yAnA7cf_A1VwT5E7NOOzUhJLVlfiTwp6u64";
+const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL as string;
+const SUPABASE_ANON_KEY = import.meta.env.VITE_SUPABASE_ANON_KEY as string;
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
